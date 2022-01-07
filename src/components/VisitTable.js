fec06053585402b62faab2272f6af4b9ff713643
lukk5 <60044256+lukk5@users.noreply.github.com>
2022-01-07 17:47:49 +0200

    Added filtering in VisitTable

--- src/components/VisitTable.js
+++ src/components/VisitTable.js
@@ -1,15 +1,80 @@
 import Link from 'next/link';
+import { fetchJson } from '../utils/HttpUtils';
+import { useState } from 'react';
+import { formatVisits } from '../utils/VisitUtils';
 
 const viewMode = process.env.NEXT_PUBLIC_VIEW_MODE;
+const userId = process.env.NEXT_PUBLIC_USER_ID;
 
 export default function VisitTable(props) {
   const doctorMode = viewMode === 'doctor' ? true : false;
-  const { visits } = props.visits;
+  const [ visits, setVisits ] = useState(props.visits.visits);
+  const [ view, setView ] = useState('Aktyvius');
+
+  const handleChangesOfView = (e) => {
+    loadByStatus(e.target.value);
+  }
+
+  async function loadByStatus(status) {
+    let items = [];
+    console.log(status);
+    if(status === 'Aktyvius') {
+      if(doctorMode) {
+        items = await fetchJson(`/Visit/get/byDoctorAndStatus?doctorId=${userId}&visitStatus=Laukiamas`);
+      } else {
+        items = await fetchJson(`/Visit/get/byPatientAndStatus?patientId=${userId}&visitStatus=Laukiamas`);
+      }
+    } else if(status === 'Patvirtintus') {
+      if(doctorMode) {
+        items = await fetchJson(`/Visit/get/byDoctorAndStatus?doctorId=${userId}&visitStatus=Patvirtintas`);
+      } else {
+        items = await fetchJson(`/Visit/get/byPatientAndStatus?patientId=${userId}&visitStatus=Patvirtintas`);
+      }
+    } else if(status === 'Atšauktus') {
+      if(doctorMode) {
+        items = await fetchJson(`/Visit/get/byDoctorAndStatus?doctorId=${userId}&visitStatus=Atšauktas`);
+      } else {
+        items = await fetchJson(`/Visit/get/byPatientAndStatus?patientId=${userId}&visitStatus=Atšauktas`);
+      }
+    } else if(status === 'Įvykdytus') {
+      if(doctorMode) {
+        items = await fetchJson(`/Visit/get/byDoctorAndStatus?doctorId=${userId}&visitStatus=Įvykęs`);
+      } else {
+        items = await fetchJson(`/Visit/get/byPatientAndStatus?patientId=${userId}&visitStatus=Įvykęs`);
+      }
+    }
+
+    const formattedVisits = await formatVisits(items).then((x) => {
+      return x;
+    });
+
+    setVisits(formattedVisits);
+  }
 
   return (
     <div className="flex flex-col">
       <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:pt-5 mb-10">
+            <label
+              htmlFor="country"
+              className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+            >
+              Rodyti
+            </label>
+            <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <select
+                onChange={handleChangesOfView}
+                id="country"
+                className="max-w-lg block focus:ring-indigo-500 focus:border-indigo-500 w-full shadow-sm sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
+              >
+                <option >Aktyvius</option>
+                <option >Patvirtintus</option>
+                <option >Atšauktus</option>
+                <option >Įvykdytus</option>
+              </select>
+            </div>
+          </div>
           <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
             <table className="min-w-full divide-y divide-gray-200">
               <thead className="bg-gray-50">
